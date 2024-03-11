f4a134c8e1dfffe128d84017e80a7e57768740d7
Thijs Kok <tkok@testmonitor.com>
2024-03-11 14:18:41 +0100

    Added ADF document parser (#25)
    * Added ADF document parser
    
    * Added ADF document parser
    
    * Added ADF document parser
    
    * Added ADF document parser
    
    * Added ADF document parser
    
    * Added ADF document parser
    
    * Added ADF document parser

---
+++ tests/DocumentParserTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace TestMonitor\Jira\Tests;
+
+use DH\Adf\Node\BlockNode;
+use PHPUnit\Framework\TestCase;
+use TestMonitor\Jira\Parsers\Adf\Document;
+use DH\Adf\Node\Block\Document as BlockDocument;
+
+class DocumentParserTest extends TestCase
+{
+    /** @test */
+    public function it_should_return_a_document_parser()
+    {
+        // Given
+
+        // When
+        $document = (new Document());
+
+        // Then
+        $this->assertInstanceOf(Document::class, $document);
+    }
+
+    /** @test */
+    public function it_should_parse_content()
+    {
+        // Given
+        $content = [
+            'type' => 'doc',
+            'version' => 1,
+            'content' => [
+                [
+                    'type' => 'paragraph',
+                    'content' => [
+                        [
+                            'type' => 'text',
+                            'text' => 'test',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        // When
+        $document = Document::load($content);
+
+        // Then
+        $this->assertInstanceOf(BlockNode::class, $document);
+        $this->assertIsArray($document->getContent());
+        $this->assertEquals((new BlockDocument)->paragraph()->text('test')->end(), $document);
+    }
+
+    /** @test */
+    public function it_should_filter_out_unknown_node_types()
+    {
+        // Given
+        $content = [
+            'type' => 'doc',
+            'version' => 1,
+            'content' => [
+                [
+                    'type' => 'paragraph',
+                    'content' => [
+                        [
+                            'type' => 'text',
+                            'text' => 'lorem',
+                        ],
+                        [
+                            'type' => 'mediaInline',
+                            'attrs' => [
+                                'id' => '12345',
+                                'collection' => '',
+                                'type' => 'file',
+                            ],
+                        ],
+                        [
+                            'type' => 'text',
+                            'text' => 'ipsum',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        // When
+        $document = Document::load($content);
+
+        // Then
+        $this->assertInstanceOf(BlockNode::class, $document);
+        $this->assertIsArray($document->getContent());
+        $this->assertEquals((new BlockDocument)->paragraph()->text('lorem')->text('ipsum')->end(), $document);
+    }
+}
