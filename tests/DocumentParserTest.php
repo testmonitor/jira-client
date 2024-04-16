e494aed9e82d8862e671301a0577b912666416bb
Thijs Kok <tkok@testmonitor.com>
2024-04-16 18:43:27 +0200

    Revised ADF parser (#31)
    * Revised ADF parser
    
    * PHPCS fix
    
    * Revised ADF parser

--- tests/DocumentParserTest.php
+++ tests/DocumentParserTest.php
@@ -3,9 +3,8 @@
 namespace TestMonitor\Jira\Tests;
 
 use DH\Adf\Node\BlockNode;
+use DH\Adf\Node\Block\Document;
 use PHPUnit\Framework\TestCase;
-use TestMonitor\Jira\Parsers\Adf\Document;
-use DH\Adf\Node\Block\Document as BlockDocument;
 
 class DocumentParserTest extends TestCase
 {
@@ -47,7 +46,7 @@ class DocumentParserTest extends TestCase
         // Then
         $this->assertInstanceOf(BlockNode::class, $document);
         $this->assertIsArray($document->getContent());
-        $this->assertEquals((new BlockDocument)->paragraph()->text('test')->end(), $document);
+        $this->assertEquals((new Document)->paragraph()->text('test')->end(), $document);
     }
 
     /** @test */
@@ -88,6 +87,6 @@ class DocumentParserTest extends TestCase
         // Then
         $this->assertInstanceOf(BlockNode::class, $document);
         $this->assertIsArray($document->getContent());
-        $this->assertEquals((new BlockDocument)->paragraph()->text('lorem')->text('ipsum')->end(), $document);
+        $this->assertEquals((new Document)->paragraph()->text('lorem')->text('ipsum')->end(), $document);
     }
 }
